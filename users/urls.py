c18ef6c39380f4496337607886c50187de84945d
Saket Mehta <saket.m17@outlook.com>
2016-04-21 23:10:41 +0530

    FInished with login, logout, pass change

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
 from . import views
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import login, logout, password_change, password_change_done
 
 urlpatterns = [
-    url(r'^accounts/login',  login),
-    url(r'^accounts/logout', logout),
-    url(r'^accounts/dashboard', views.dashboard, name='dashboard'),
+    url(r'^accounts/login/$',  login, name='login'),
+    url(r'^accounts/logout/$', logout, name='logout'),
+    url(r'^accounts/password_change/done/$', password_change_done, name='password_change_done'),
+    url(r'^accounts/password_change/$', password_change, {'post_change_redirect': 'done/'}, name='password_change'),
+    url(r'^accounts/dashboard/$', views.dashboard, name='dashboard'),
 ]
